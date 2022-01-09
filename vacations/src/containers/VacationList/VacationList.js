cf62010c09607c36db5296607b319a4acb94d061
ofer221 <ofer2212@gmail.com>
2022-01-09 18:44:54 +0200

    Create vacation list component

---
+++ vacations/src/containers/VacationList/VacationList.js
@@ -0,0 +1,35 @@
+import React, {Fragment} from 'react';
+import Grid from '@mui/material/Grid';
+import PropTypes from 'prop-types';
+import VacationCard from '../../components/VacationCard/VacationCard';
+
+function VacationList(props) {
+  const {vacations} = props;
+
+  return (
+    <>
+      {vacations.map(vacation => (
+        <Grid item>
+          <VacationCard
+            onEditClick={() => {}}
+            onDeleteClick={() => {}}
+            vacationData={vacation}
+          />
+        </Grid>
+      ))}
+    </>
+  );
+}
+
+VacationList.propTypes = {
+  vacations: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      location: PropTypes.string.isRequired,
+      price: PropTypes.string.isRequired,
+      imageUrl: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+};
+
+export default VacationList;
