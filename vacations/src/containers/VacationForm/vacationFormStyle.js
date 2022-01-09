f32c0deb39784c61bbba5b945195f47969dd2d6e
ofer221 <ofer2212@gmail.com>
2022-01-09 18:47:47 +0200

    Create vacation form component

---
+++ vacations/src/containers/VacationForm/vacationFormStyle.js
@@ -0,0 +1,42 @@
+const vacationFormStyle = (theme) => ({
+  root: {
+    maxWidth: 618,
+    minWidth: 200,
+    width: 618,
+    height: 552,
+    marginTop: "84px",
+    borderRadius: "25px",
+    position: "relative",
+    padding: "13px 40px 0px",
+    textAlign: "center",
+  },
+  formBackground: {
+    backgroundImage: `url(${require("../../assets/images/form_bg.png")})`,
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center bottom",
+  },
+  formRoot: {
+    marginTop: "27px",
+    "& .MuiFormControl-root": {
+      marginBottom: 25,
+      width: "100%",
+    },
+  },
+  btnsContainer: {
+    width: "132px",
+    display: "flex",
+    position: "relative",
+    bottom: 34,
+    marginLeft: "auto",
+    marginRight: "auto",
+    zIndex: 2,
+  },
+  editBtnsStyle: {
+    justifyContent: "space-between",
+  },
+  addBtnStyle: {
+    justifyContent: "center",
+  },
+});
+
+export default vacationFormStyle;
