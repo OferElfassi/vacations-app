a8d4b6426b9399c0e9ca46937c8f3a9c8bc6da30
ofer221 <ofer2212@gmail.com>
2022-01-09 18:52:32 +0200

    testing the new app layout component

--- vacations/src/App.js
+++ vacations/src/App.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import SearchBar from './containers/SearchBar/SearchBar';
+import VacationForm from './containers/VacationForm/VacationForm';
+import VacationList from './containers/VacationList/VacationList';
+import AppLayout from './containers/AppLayout/AppLayout';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+      <AppLayout
+          searchBar={<SearchBar />}
+          vacationForm={<VacationForm />}
+          vacationList={
+            <VacationList
+                vacations={[
+                  {
+                    name: 'Kudahuvadhoo Island',
+                    location: 'Maldives',
+                    price: '1,480',
+                    imageUrl: require('./assets/images/vacation2.png'),
+                  },
+                  {
+                    name: 'Kudahuvadhoo Island',
+                    location: 'Maldives',
+                    price: '1,480',
+                    imageUrl:
+                        'https://pbs.twimg.com/profile_images/712703916358537217/mcOketun_400x400.jpg',
+                  },
+                ]}
+            />
+          }
+      />
+  );
 }
 
 export default App;
