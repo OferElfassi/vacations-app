f3c8d149eac18e9267f0cd994566d66f47b1aae8
ofer221 <ofer2212@gmail.com>
2022-01-10 17:37:55 +0200

    crating use array hook to handle vacations array

---
+++ vacations/src/hooks/useArray.js
@@ -0,0 +1,23 @@
+import {useState} from 'react';
+
+const useArray = initialArray => {
+  const [array, updateArray] = useState(initialArray);
+
+  const addItem = itemData => {
+    updateArray(prevState => [...prevState, itemData]);
+  };
+
+  const editItem = itemData => {
+    updateArray(
+      array.map(item => (item.id === itemData.id ? {...itemData} : item)),
+    );
+  };
+
+  const deleteItem = itemId => {
+    updateArray(array.filter(item => item.id !== itemId));
+  };
+
+  return [array, addItem, editItem, deleteItem];
+};
+
+export default useArray;
