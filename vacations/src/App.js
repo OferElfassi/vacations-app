8972ead3a097c15068b79a4b6b6af6f33155ebd5
ofer221 <ofer2212@gmail.com>
2022-01-10 17:38:29 +0200

    using the array hook

--- vacations/src/App.js
+++ vacations/src/App.js
@@ -1,27 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
 import SearchBar from './containers/SearchBar/SearchBar';
 import VacationForm from './containers/VacationForm/VacationForm';
 import VacationList from './containers/VacationList/VacationList';
 import AppLayout from './containers/AppLayout/AppLayout';
 import vacationsArray from './data/data';
+import useArray from './hooks/useArray';
 
 function App() {
-  // vacationArray
-  const handleEditClick = vacationId => {};
-  const handleDeleteClick = vacationId => {};
-  return (
-    <AppLayout
-      searchBar={<SearchBar />}
-      vacationForm={<VacationForm />}
-      vacationList={
-        <VacationList
-          vacations={vacationsArray}
-          onDeleteClick={handleDeleteClick}
-          onEditClick={handleEditClick}
+    const [vacationEdit, setVacationEdit] = useState(null);
+    const [vacations, addVacation, editVacation, deleteVacation] =
+        useArray(vacationsArray);
+
+    const handleEditClick = vacationId => {
+        const fetchedVacation = vacations.find(vac => vac.id === vacationId);
+        setVacationEdit(fetchedVacation);
+    };
+
+    const handleFormCancelClick = () => {
+        setVacationEdit(null);
+    };
+    return (
+        <AppLayout
+            searchBar={<SearchBar />}
+            vacationForm={
+                <VacationForm
+                    onSubmitClick={addVacation}
+                    onCancelClick={handleFormCancelClick}
+                    onEditClick={editVacation}
+                    initialValues={vacationEdit}
+                    editMode={vacationEdit !== null}
+                />
+            }
+            vacationList={
+                <VacationList
+                    vacations={vacations}
+                    onDeleteClick={deleteVacation}
+                    onEditClick={handleEditClick}
+                />
+            }
         />
-      }
-    />
-  );
+    );
 }
 
 export default App;
